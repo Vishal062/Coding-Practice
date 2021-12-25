e881a8ed1e0ae60f4b1086df840b4eb38cbc4bb5
Vishal062 <vs9425348@gmail.com>
2021-12-26 01:28:54 +0530

    Testing Api

--- RevisionDay-3/_testApi/script.js
+++ RevisionDay-3/_testApi/script.js
@@ -2,13 +2,14 @@
 
 let searchMarvel = (query) => {
     const url = `https://api.tvmaze.com/search/shows?q=${query}`;
+    //const url = `https://swapi.dev/api/people/?search=${query}`;
     fetch(url)
     .then(res => res.json())
     .then((jsonData) => {
         //since jsondata is in array we use map function to get details
         //in web page
         const result = jsonData.map(element => element.show.name);
-        //console.log(result);
+        console.log(result);
         //in console you can see the result
         //but Now we check for render result in web Page >>
         renderResult(result);
@@ -45,3 +46,8 @@ window.onload =() =>{
         //searchMarvel(searchFieldData.value);
     }
 }
+
+//marvel api ? 5681cccfb95effd8448fedba5f11202ff2e79b70
+//public key ? afc073d226e7a73d88a572d0d1f9cab6
+//marvel api
+//https://gateway.marvel.com/v1/public/characters?ts=thesoer&apikey=001ac6c73378bbfff488a36141458af2&hash=72e5ed53d1398abb831c3ceec263f18b
