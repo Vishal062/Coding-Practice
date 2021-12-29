00ab02e4c6584c93d8a2002891a2023595c6b539
Vishal062 <vs9425348@gmail.com>
2021-12-29 22:20:46 +0530

    Hoisting Concept

--- NamasteJS/Hoisting/hoisting.js
+++ NamasteJS/Hoisting/hoisting.js
@@ -28,14 +28,41 @@
 
 //Example 3
 
+// hello();
+// console.log(x);
+// console.log(hello);
+
+// var x =7;
+
+// function hello(){
+//     console.log("Hello Hoisting");
+// }
+//This is very weird for the x its giving undefined but for the function its giving 
+//the correct output.
+
+//Example 4 use of arrow function >
+
 hello();
 console.log(x);
 console.log(hello);
 
 var x =7;
 
-function hello(){
+
+var hello = () =>{
     console.log("Hello Hoisting");
 }
-//This is very weird for the x its giving undefined but for the function its giving 
-//the correct output.
+
+//Another type of writting function > 
+
+var hello = function (){
+    console.log("Hello new function");
+}
+//Now this will give you error due to arrow function.
+//This time hello function work as a another variable in global scope and its value
+//assigned as a undefined.
+//and arrow function and foo = function(){...} type of function 
+//also behave like a variable in memory creaton phase
+
+//Variables are partially hoisted while functions are fully hoisted.
+
