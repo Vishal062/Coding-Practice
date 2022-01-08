7c0b2ce7947f4bcba64a878d954490ea7080a9b2
Vishal062 <vs9425348@gmail.com>
2022-01-08 19:47:20 +0530

    Exa Clo & debou

--- NamasteJS/Closure/script.js
+++ NamasteJS/Closure/script.js
@@ -1,43 +1,22 @@
-//Closure , What are closure.
-//Why we do need to use closure.
-//What is currying?
-//Application
-
-//A mechanism in which js maintain scope of some variable in closure scope, if the
-//particular variable goes out of scope 
-//js pass 2 engine, lexical scope if matters more on where your function are declared
-//Author time.
-
-//Closure >>>>>>
-
-// function outer() {
-//     var a =10;
-//     return function inner(b){
-//         return a+b;
-//     }
-// }
-// var fn = outer();
-// console.log(fn(5));
 
 //debouncing >>>>>
 
-// const makeApicall = () => {
-//     console.log("api call in progress");
-// }
+const makeApicall = () => {
+    console.log("api call in progress");
+}
 
-// const debouncer = () => {
-//     let debouncing;
-//     //
-//     return function () {
-//         debouncing && clearTimeout(debouncing);
-//         debouncing = setTimeout(()=> func.apply(this,arguments), delay)
-//     }
-// }
-// window.addEventListener("load",function(){
-//     var btn = document.getElementById('click');
+const debouncer = () => {
+    let debouncing;
+    return function () {
+        debouncing && clearTimeout(debouncing);
+        debouncing = setTimeout(()=> func.apply(this,arguments), delay)
+    }
+}
+window.addEventListener("load",function(){
+    var btn = document.getElementById('click');
     
-//     btn.addEventListener("click",debouncer(makeApicall,1000))
-// });
+    btn.addEventListener("click",debouncer(makeApicall,1000))
+});
 
 
 //Throttling: which will gaurenteed a call after n second.
