76b1ef4b42b59e159165292dea10a7fea22229b4
Vishal062 <vs9425348@gmail.com>
2022-01-16 16:19:54 +0530

    Event loop Ex

--- NamasteJS/EventLoop/script.js
+++ NamasteJS/EventLoop/script.js
@@ -1,17 +1,17 @@
 function execute(){
     console.log("Execution 1");
 
-    var p = new Promise((resolve) => {
+    var p = new Promise((resolve) => {  //fun A
         setTimeout(()=> resolve(2),5000);
     });
 
-    p.then(console.log);
+    p.then(console.log);    //Once the above promise resolved after 5 sec then it console
 
-    setTimeout(() => {
+    setTimeout(() => {      //fun B
         console.log("Execution zero delay");
     },1000);
 
-    Promise.resolve(4).then(console.log);
+    Promise.resolve(4).then(console.log);   //fun C
 
     console.log("Last execution");
 }
@@ -19,3 +19,4 @@ function execute(){
 execute();
 
 //exe1, last exec, 4, execu zero delay, 2;
+//Promise is always send to the microtask queue.
