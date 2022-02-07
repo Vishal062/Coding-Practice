a66db828ffbff209729ab5b27e3ba1d3441ab168
Vishal062 <vs9425348@gmail.com>
2022-02-07 19:45:33 +0530

    Function Details

--- NamasteJS/callBackFunction/script.js
+++ NamasteJS/callBackFunction/script.js
@@ -14,3 +14,27 @@ function closureex(){
 }
 
 closureex()
+
+
+
+//Callback___Async await___Promise>>>>>>
+
+/*
+These are used to acheive asynchronous Programming in javascript.
+
+The callback is the oldestr way to work with asynchronous operation in js
+The callback is a function passed to another function as an arguments
+
+The callback suffers from the callback hell problem when working with
+dependent asunchronous calls.
+It offers poor error handling
+The promise is the new way of dealing with asunchronous operations and 
+it is introduced in ES6.
+
+It overcomes the problem of callback.
+
+The promise offers diffrent set of methods to work with dependent and parallel
+asynchronous calls.
+It provides better error handling than the callback.
+
+*/
